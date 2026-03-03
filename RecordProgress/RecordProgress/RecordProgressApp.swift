71b4ba7b41e83884746a058b6a304ffd4c0190c9
Alexander Shevchenko <47176719+AleksandrShevachenko@users.noreply.github.com>
2026-03-03 15:25:14 +0200

    Add files via upload

---
+++ RecordProgress/RecordProgress/RecordProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordProgressApp.swift
+//  RecordProgress
+//
+//  Created by admin on 02.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct RecordProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
